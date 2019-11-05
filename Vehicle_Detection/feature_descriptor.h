db981258bc9b03a05503f0bf108a4fd5e47c5ddc
gohurali <gohurali@live.com>
2019-11-05 01:26:04 -0800

    reads input img

--- Vehicle_Detection/feature_descriptor.h
+++ Vehicle_Detection/feature_descriptor.h
@@ -1,6 +1,8 @@
 #ifndef FEATURE_DESCRIPTOR_H
 #define	FEATURE_DESCRIPTOR_H
-
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <stdio.h>
 class FeatureDescriptor {
 public:
 private:
--- Vehicle_Detection/main.cpp
+++ Vehicle_Detection/main.cpp
@@ -1,5 +1,19 @@
 #include "feature_descriptor.h"
+
+void show_image(cv::Mat& img, int x_window, int y_window, int delay) {
+	cv::namedWindow("output", cv::WINDOW_NORMAL);
+	cv::resizeWindow("output", img.cols / x_window, img.rows / y_window);
+	cv::imshow("output", img);
+	cv::waitKey(delay);
+}
+
 int main() {
+	bool debug = true;
+
+	cv::Mat input = cv::imread("images/input.jpg");
+	if (debug)
+		printf("Input img size (r,c) %i x %i\n",input.rows,input.cols);
+		show_image(input, 1, 1, 5000);
 
 	return 0;
 }
